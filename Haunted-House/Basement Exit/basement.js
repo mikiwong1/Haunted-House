80a91c69fbeb25f16aad8ad8f16809487fa0d8f8
AbdullahA06 <114007132+AbdullahA06@users.noreply.github.com>
2023-11-10 06:02:33 -0500

    Add files via upload

--- Haunted-House/Basement Exit/basement.js
+++ Haunted-House/Basement Exit/basement.js
@@ -6,6 +6,30 @@ var elevator = document.getElementById("elevator");
 var staircase2 = document.getElementById("staircase2");
 var elevator2 = document.getElementById("elevator2");
 var stairfail = document.getElementById("retrystairs");
+var firstfloor = document.getElementById("floor");
+var running = document.getElementById("run");
+var stilling = document.getElementById("still");
+var backing = document.getElementById("back");
+var fwrd = document.getElementById("forward");
+var rght = document.getElementById("right");
+var ctch = document.getElementById("retry");
+var feeling = document.getElementById("feel");
+var back = document.getElementById("backhall");
+var breakA = document.getElementById("breakaway");
+var careF = document.getElementById("careful");
+var runnin = document.getElementById("congrats");
+
+
+
+
+
+setTimeout(function right(){
+    fwrd.style.visibility = "visible";
+}, 8200);
+
+setTimeout(function forward(){
+    rght.style.visibility = "visible";
+}, 8400);
 
 setTimeout(function loose(){
     loosen.style.visibility = "visible";
@@ -39,3 +63,63 @@ setTimeout(function elevate2(){
 setTimeout(function fall(){
     stairfail.style.visibility = "visible";
 }, 11110);
+
+setTimeout(function nextRoom(){
+    firstfloor.style.visibility = "visible";
+}, 8200);
+
+setTimeout(function loose(){
+    loosen.style.visibility = "visible";
+}, 8200);
+
+setTimeout(function cut(){
+    pipe.style.visibility = "visible";
+}, 8400);
+
+
+setTimeout(function lazier(){
+    lazy.style.visibility = "visible";
+}, 8600);
+
+
+setTimeout(function run(){
+    running.style.visibility = "visible";
+}, 8600);
+
+setTimeout(function still(){
+    stilling.style.visibility = "visible";
+}, 8200);
+
+
+setTimeout(function back(){
+    backing.style.visibility = "visible";
+}, 8400);
+
+setTimeout(function caught(){
+    ctch.style.visibility = "visible";
+}, 2000);
+
+
+setTimeout(function backhall(){
+    back.style.visibility = "visible";
+}, 8400);
+
+setTimeout(function dark(){
+    feeling.style.visibility = "visible";
+}, 8200);
+
+setTimeout(function breakAway(){
+    breakA.style.visibility = "visible";
+}, 8200);
+
+setTimeout(function careFul(){
+    careF.style.visibility = "visible";
+}, 8400);
+
+setTimeout(function congrats(){
+    runnin.style.visibility = "visible";
+}, 8200);
+
+
+
+
